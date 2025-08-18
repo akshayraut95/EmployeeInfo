85cdf361627a5ce057c1990fa5e870ba3da8bc2f
akshay <akshayraut080@gmail.com>
2025-08-18 11:56:27 +0530

    added repository

---
+++ EmployeeInfo/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,65 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.module.Employee;
+import com.example.demo.service.EmployeeService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeService employeeService;
+
+      @PostMapping("/save")
+      public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee) {
+    //TODO: process POST request
+    Employee emp=employeeService.saveEmployee(employee);
+      return ResponseEntity.ok().body(emp);
+     }
+
+       @GetMapping("/employeeslist")
+       public List<Employee> getEmployees() {
+       return employeeService.getEmployees();
+       }
+
+       @GetMapping("/{id}")
+       public Employee getEmployeeById(@PathVariable int id) {
+           
+    	   return employeeService.getEmployeeById(id); 
+       }
+       
+       
+      @PutMapping("/{id}")
+      public Employee updateEmployeeById(@PathVariable int id, @RequestBody Employee employee) {
+          //TODO: process PUT request
+          
+          return employeeService.updateEmployeeById(id, employee);
+      }
+      
+      
+      @DeleteMapping("/{id}")
+  	public void deleteEmployeeById(@PathVariable int id) {
+    	  
+  		 employeeService.deleteEmployeeById(id);
+  		 
+  		 System.out.println("Deleted Succefully...");
+  		
+
+}
+}
---
+++ EmployeeInfo/src/main/java/com/example/demo/module/Employee.java
@@ -0,0 +1,58 @@
+package com.example.demo.module;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String firstname;
+	private String lastname;
+	private String email;
+	private String phone;
+	private String address;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
